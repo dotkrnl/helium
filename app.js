0aa4889bb5ad4bcc23e8a2723be2ea3326abe829
Jason Lau <dotkrnl@gmail.com>
2013-08-31 12:38:38 +0800

    change server name

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('MSVA server listening on port ' + app.get('port'));
 });
