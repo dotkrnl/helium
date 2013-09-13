7af91d24e1d416f1c19b7d3c0fb2736356d8108d
Jason Lau <i@dotkrnl.com>
2013-09-13 22:08:23 +0800

    new example setting

---
+++ settings.example.js
@@ -0,0 +1,25 @@
+
+/**
+ * App settings.
+ */
+
+var express = require('express');
+var mongoStore = require('connect-mongo')(express);
+
+var website = '志愿者协会';
+var defaultPort = 3000;
+var cookieSecret = "secretcode";
+var databaseInfo = {
+    db: 'helium',
+}
+
+module.exports = {
+    website: website,
+    defaultPort: defaultPort,
+    secret: cookieSecret,
+    databaseInfo: databaseInfo,
+    sessionDB: {
+        secret: cookieSecret,
+        store: new mongoStore(databaseInfo),
+    },
+};
