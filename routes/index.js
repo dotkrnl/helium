eccea1f2a70f425ebd377b95354ab3d0ab8236be
Jason Lau <i@dotkrnl.com>
2013-09-16 22:45:03 +0800

    temporary disable /user for nonadmin user. for issue on Second Conference.

--- routes/index.js
+++ routes/index.js
@@ -34,8 +34,8 @@ function ensurePermission(req, res, next) {
 module.exports = function(app) {
     app.get('/', homepage.index);
 
-    app.get('/user', user.showList);
-    app.get('/user/page/:page(\\d+)', user.showList);
+    app.get('/user', ensureAdmin,  user.showList);
+    app.get('/user/page/:page(\\d+)', ensureAdmin, user.showList);
     app.get('/user/register', user.showRegister);
     app.post('/user/register', user.doRegister);
     app.get('/user/signin', user.showSignin);
